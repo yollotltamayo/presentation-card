be3c886628bc95da76a1b73152746891accd2634
yollotltamayo <yollotltamayo@comunidad.unam.mx>
2020-08-18 23:00:05 -0500

    Updates

---
+++ precache-manifest.5c0806799cf54410ec3f209c64d35dc5.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "372f5c5083837d9ace7032a3b91ff14d",
+    "url": "/presentation-card/index.html"
+  },
+  {
+    "revision": "72c2453bc0490d2ddc08",
+    "url": "/presentation-card/static/css/2.6176cce6.chunk.css"
+  },
+  {
+    "revision": "8fa86972cc36b7659ceb",
+    "url": "/presentation-card/static/css/main.e2a064df.chunk.css"
+  },
+  {
+    "revision": "72c2453bc0490d2ddc08",
+    "url": "/presentation-card/static/js/2.d852d013.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/presentation-card/static/js/2.d852d013.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fa86972cc36b7659ceb",
+    "url": "/presentation-card/static/js/main.938b885b.chunk.js"
+  },
+  {
+    "revision": "c276c35dfcfefc0a5534",
+    "url": "/presentation-card/static/js/runtime-main.f56a3020.js"
+  },
+  {
+    "revision": "b1a050dbfada162098d26e73a9d37a4f",
+    "url": "/presentation-card/static/media/custom.b1a050db.png"
+  },
+  {
+    "revision": "f34557184984b6e95dc1d6d28fcb54e6",
+    "url": "/presentation-card/static/media/github.f3455718.jpg"
+  },
+  {
+    "revision": "8f9cc16567102f17f6cd3900a71e1bd0",
+    "url": "/presentation-card/static/media/img4.8f9cc165.JPG"
+  },
+  {
+    "revision": "ba0c3e62d7437bd1712d9cc621109ef7",
+    "url": "/presentation-card/static/media/img5.ba0c3e62.jpeg"
+  },
+  {
+    "revision": "d94a163a5eb97c12061ef75e3069e26d",
+    "url": "/presentation-card/static/media/lil_cat.d94a163a.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/presentation-card/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8a792130904e46762622c56d5688a65b",
+    "url": "/presentation-card/static/media/metodos_numericos.8a792130.png"
+  },
+  {
+    "revision": "5c3d61353a53d3099b343a5f616ec57a",
+    "url": "/presentation-card/static/media/resume.5c3d6135.pdf"
+  },
+  {
+    "revision": "003cbb8a1be0b65b8a69ebee0f974591",
+    "url": "/presentation-card/static/media/teoria_grafo.003cbb8a.png"
+  }
+]);
